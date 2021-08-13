4ade84bf0798918bc27497ab608f45b5c595b680
Berto Furth <bertofurth@sent.com>
2021-08-14 09:38:47 +1000

    Make run_all_build.sh more flexible

--- run_all_build.sh
+++ run_all_build.sh
@@ -14,19 +14,9 @@ checkerr()
     fi
 }
 
-./build-samba-01-gmp.sh
+foreach script_name ( $(ls build-samba*.sh) )
+./$script_name
 checkerr
-./build-samba-02-nettle.sh
-checkerr
-./build-samba-03-acl.sh
-checkerr
-./build-samba-04-libtasn1.sh
-checkerr
-./build-samba-05-gnutls.sh
-checkerr
-./build-samba-06-openldap.sh
-checkerr
-./build-samba-07-samba-host-tools.sh
-checkerr
-./build-samba-08-samba.sh
-checkerr
+end
+
+
