f768b28482f782a5e1d45ed8e01a33a9d90ad982
Berto Furth <bfgithub@veryspeedy.net>
2022-03-09 09:45:53 +1100

    No systemd on Seagate Central

--- build-samba-09-p11-kit.sh
+++ build-samba-09-p11-kit.sh
@@ -6,13 +6,14 @@ change_into_obj_directory
 
 #
 # --without-libffi : Disable foreign language functions.
-#
+# --without-systemd  : No systemd on Seagate Central
 
 configure_it --prefix=$PREFIX \
 	     --bindir=$EXEC_PREFIX/bin \
 	     --sbindir=$EXEC_PREFIX/sbin \
 	     --host=$ARCH \
-	     --without-libffi
+	     --without-libffi \
+	     --without-systemd
 make_it
 install_it
 finish_it
